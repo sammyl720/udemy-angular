ec7cf6954b75ff6fd52a6361e62d008e04f94d88
sam leider <sam.leider@hotmail.com>
2020-02-04 19:14:59 -0500

    ngClass directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: ['./server.component.css']
+  styles: [`
+    .online {
+      color:white;
+    }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
